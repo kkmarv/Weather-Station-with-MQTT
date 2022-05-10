2aa558719b3cfc75bc6594d95dc197b696e10285
Kästing, Marvin <marvin.kaesting@student.hs-anhalt.de>
2022-05-10 12:36:55 +0000

    Update infrared.cpp

--- src/2) sensors/infrared.cpp
+++ src/2) sensors/infrared.cpp
@@ -2,27 +2,33 @@
 
 #define IRPIN D8
 
-void IRSetup() {
+void calibrate() {
   int seconds = 90;
-  Serial.begin(9600);
-  pinMode(IRPIN, INPUT);
   for (size_t i = 0; i < 18; i++) {
-    Serial.printf("\n");
-    Serial.printf("Calibrating... (%d)", seconds);
+    Serial.printf("Calibrating... (%ds left)\r", seconds);
     seconds -= 5;
     delay(5000);
   }
-  Serial.println("\nCalibration done. Searching...");
+  Serial.println("Calibration done. Searching...");
+}
+
+void IRSetup() {
+  Serial.begin(9600);
+  pinMode(IRPIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println();
+  calibrate();
 }
 
 void readIR() {
   if (digitalRead(IRPIN) == HIGH) {
-    Serial.println("Motion spotted!");
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print("Motion spotted!\r");
+    digitalWrite(LED_BUILTIN, LOW);  // led on
     delay(2000);
-    digitalWrite(LED_BUILTIN, LOW);
   } else {
-    Serial.println("No Motion.");
+    Serial.print("No Motion.     \r");
   }
   delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);  // led off
 }
