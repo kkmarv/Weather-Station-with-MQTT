f052e37a733c9819d43596aef96961f73e7e0500
Marvin <marvin.kaesting@student.hs-anhalt.de>
2022-09-22 00:00:23 +0200

    add mqtt publishing

---
+++ include/config.h
@@ -0,0 +1,7 @@
+#define WIFI_SSID ""  // WIFI SSID
+#define WIFI_PASS ""  // WIFI passphrase
+
+#define MQTT_BROK ""    // URL or IP address pointing towards the MQTT Broker.
+#define MQTT_PORT 1883  // The port on which the Broker listens. Default is 1883.
+#define MQTT_USER ""    // The client ID for authentication with the MQTT Broker.
+#define MQTT_PASS ""    // The password for authentication with the MQTT Broker.
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <Arduino.h>
+#include "utils.h"
+#include "wifi.h"
 
 // #include "blink.h"
 // #include "buzz.h"
 // #include "humtemp.h"
-#include "infrared.h"
+// #include "infrared.h"
 // #include "ultraSound.h"
 
 void setup() {
+  startSerialComs();
+  connectToWifi();
+  connectToMQTT();
+
   // setupDHT()
   // setupUltraSound(D6, D7);
-  IRSetup();
+  // IRSetup();
 }
 
 void loop() {
-  // readDHT
-  // readDistance();
-  readIR();
+  publishMessagePeriodically("test", "Bapanada", 10000);
 }
